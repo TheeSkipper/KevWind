56706972d160e7d4ca3fe957b5451587dd97b826
Skip Jones <skip.wjones@gmail.com>
2018-01-31 16:41:53 -0600

    15

--- js/map.js
+++ js/map.js
@@ -10,14 +10,14 @@
      // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
      var mapOptions = {
          // How zoomed in you want the map to start at (always required)
-         zoom: 12,
+         zoom: 13,
 
          // The latitude and longitude to center the map (always required)
          center: new google.maps.LatLng(38.704004, -90.290914), // New York
 
          // How you would like to style the map.
          // This is where you would paste any style found on Snazzy Maps.
-         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road","elementType":"all","stylers":[{"weight":"2"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":17}]}]
+         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#2E535A"},{"lightness":100}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#2E535A"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#2E535A"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":5}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":21}]},{"featureType":"road","elementType":"all","stylers":[{"weight":"2"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#2E535A"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#2E535A"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2E535A"},{"lightness":17}]}]
      };
 
      // Get the HTML DOM element that will contain your map
@@ -28,7 +28,7 @@
      var map = new google.maps.Map(mapElement, mapOptions);
 
      // Let's also add a marker while we're at it
-     var iconBase = 'img/6Asset 1.svg';
+     var iconBase = 'img/favicon-144.svg';
      var marker = new google.maps.Marker({
          position: new google.maps.LatLng(38.704004, -90.290914),
          map: map,
